2ee61c33a0fe5e86b64624b5def6620913e7cb4e
Yu Tao <yutao1016@gmail.com>
2016-09-29 22:55:12 +0800

    添加了题目

--- 001.py
+++ 001.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+
+# 第 0001 题：做为 Apple Store App 独立开发者，你要搞限时促销，为你的应用生成激活码（或者优惠券），
+# 使用 Python 如何生成 200 个激活码（或者优惠券）？
 import random,string
 
 def gen_code(num):
