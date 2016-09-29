6d3e2d0cddb1d0e46253bc12cda04f4b53c38bdd
Yu Tao <yutao1016@gmail.com>
2016-09-29 22:24:51 +0800

    delete the comment

--- 0016.py
+++ 0016.py
@@ -1,22 +1,3 @@
-# import xlsxwriter, json
-#
-# def txt2xlsw():
-#     wb = xlsxwriter.Workbook('numbers.xls')
-#     ws = wb.add_worksheet('numbers')
-#
-#     with open('numbers.txt') as f:
-#         data = json.load(f)
-#         for i in range(len(data)):
-#             for j in range(len(data[i])):
-#                 ws.write(i, j, data[i][j])
-#     wb.close()
-#
-# if __name__ == "__main__":
-#     txt2xlsw()
-
-#!/usr/bin/env python3
-#from json to xlsx
-
 import xlsxwriter, json
 
 def json2xls():
